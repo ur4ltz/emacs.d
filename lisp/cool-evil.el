d11d8ce5c92a6c3f8de503c4af97ca6500bd16a8
Andy Shevchenko <ur4ltz@gmail.com>
2020-10-01 12:04:31 +0300

    Updated settings 'evil'.

--- lisp/cool-evil.el
+++ lisp/cool-evil.el
@@ -8,25 +8,31 @@
 (use-package evil
   :ensure t
   :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
+  (setq evil-normal-state-cursor '("DarkGoldenrod2" box)
+	evil-insert-state-cursor '("chartreuse3" (bar . 2))
+	evil-emacs-state-cursor '("SkyBlue2" box)
+	evil-replace-state-cursor '("red" (hbar . 2))
+	evil-visual-state-cursor '("gray" (hbar . 2))
+	evil-motion-state-cursor '("plum3" box)
+	evil-want-integration t
+	evil-want-C-u-scroll t)
   :config
-  (evil-mode 1)
+  (evil-mode 1))
 
-  (use-package evil-magit
-    :ensure t
-    :config
-    (require 'magit)
-    (setq evil-magit-state 'normal)
-    (setq evil-magit-use-y-for-yank nil))
+(use-package evil-magit
+  :ensure t
+  :config
+  (require 'magit)
+  (setq evil-magit-state 'normal)
+  (setq evil-magit-use-y-for-yank nil))
 
-  (use-package evil-surround
-    :ensure t
-    :config
-    (global-evil-surround-mode))
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode))
 
-  (use-package evil-nerd-commenter
-    :ensure t))
+(use-package evil-nerd-commenter
+  :ensure t)
 
 (use-package evil-collection
   :after evil
