ba3d34591f4a9c6efe0dfa4cc8b74a0c9068fd14
Andy Shevchenko <ur4ltz@gmail.com>
2021-02-03 13:13:06 +0200

    Use diff-hl.

--- lisp/cool-magit.el
+++ lisp/cool-magit.el
@@ -10,5 +10,12 @@
 	    (lambda ()
 	      (define-key magit-mode-map (kbd ",o") 'delete-other-windows))))
 
+(use-package diff-hl
+  :ensure t
+  :config
+  (global-diff-hl-mode 1)
+  (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+
 (provide 'cool-magit)
 ;;; cool-magit.el ends here
