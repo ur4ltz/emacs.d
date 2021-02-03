71d1bc2088fecd135e7a88c37b37da0573ff416c
Andy Shevchenko <ur4ltz@gmail.com>
2021-02-03 13:18:09 +0200

    Update dashboard config.

--- lisp/cool-dashboard.el
+++ lisp/cool-dashboard.el
@@ -8,6 +8,12 @@
 (use-package dashboard
   :ensure t
   :config
+  (setq dashboard-items	'((recents . 5)
+			  (bookmarks . 5)
+			  (projects . 5)
+			  (registers . 5)))				   
+  (setq dashboard-set-heading-icons t
+	dashboard-set-file-icons t)   
   (dashboard-setup-startup-hook))
 
 (provide 'cool-dashboard)
