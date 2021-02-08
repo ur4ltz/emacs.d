0c6ead4195fc33e9f6aeacdc0d7d88d1ebcc34b8
Andy Shevchenko <ur4ltz@gmail.com>
2021-02-08 05:23:40 +0200

    Added .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-project-compilation-cmd . "make build")
+         (projectile-project-run-cmd . "emacs")
+         (projectile-project-name . "dotemacs"))))
