5f07e772827a7155ba564c857a6f006978dbcf32
Andy Shevchenko <ur4ltz@gmail.com>
2021-02-11 21:08:22 +0200

    Update lua-mode config.

--- lisp/cool-lua.el
+++ lisp/cool-lua.el
@@ -5,12 +5,15 @@
 
 (use-package lua-mode
   :ensure t
-  :mode "\\.lua$"
+  :mode
+  ("\\.lua\\'")
+  :hook
+  (lua-mode . lsp-deferred)
   :interpreter "lua"
   :config
-  (setq lua-indent-level 4)
-  (setq lua-indent-string-contents t)
-  (setq lua-prefix-key nil))
+  (setq lua-indent-level                 4
+        lua-indent-string-contents       t
+        lua-prefix-key                   nil))
 
 (provide 'cool-lua)
 ;;; cool-lua.el ends here
