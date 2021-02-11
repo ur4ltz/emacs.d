5e03df75f2fe0e9ec2878e23eb68e3871755ff9e
Andy Shevchenko <ur4ltz@gmail.com>
2021-02-11 21:08:00 +0200

    Update lsp config.

--- lisp/cool-lsp.el
+++ lisp/cool-lsp.el
@@ -1,5 +1,6 @@
-;;; cool-lsp.el -*- lexical-binding: t -*-
+;;; cool-lsp.el --- summary -*- lexical-binding: t -*-
 
+;;; Commentary:
 ;;; Code:
 
 (use-package lsp-mode
@@ -7,15 +8,22 @@
   :init
   (setq lsp-keymap-prefix "C-c l")
   :hook (
-	 (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp-mode)
 
 (use-package lsp-ui
   :ensure t
-  :commands lsp-ui-mode)
+  :commands lsp-ui-mode
+  :hook
+  (lsp-mode . lsp-ui-mode))
+
+(use-package lsp-ivy
+  :ensure t
+  :commands lsp-ivy-workspace-symbol)
 
 (use-package lsp-treemacs
   :ensure t
+  :after lsp-mode
   :commands lsp-treemacs-error-list)
 
 (provide 'cool-lsp)
