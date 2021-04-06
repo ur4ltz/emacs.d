d2e7053ab85a090f56feb8441a45df4a31b3fc87
Andy Shevchenko <ur4ltz@gmail.com>
2021-04-06 23:50:13 +0300

    Update dashboard config.

--- lisp/cool-dashboard.el
+++ lisp/cool-dashboard.el
@@ -1,5 +1,6 @@
-;;;
+;;; cool-dashboard.el --- summary -*- lexical-binding: t -*-
 
+;;; Commentary:
 ;;; Code:
 
 (use-package all-the-icons
@@ -8,12 +9,16 @@
 (use-package dashboard
   :ensure t
   :config
-  (setq dashboard-items	'((recents . 5)
-			  (bookmarks . 5)
-			  (projects . 5)
-			  (registers . 5)))
-  (setq dashboard-set-heading-icons t
-	dashboard-set-file-icons t)
+  (setq dashboard-items	'((recents   . 10)
+                          ;;(bookmark  . 5)
+                          (projects  . 10)
+                          ;;(registers . 10)
+                          ))
+
+  (setq dashboard-set-init-info     t
+        dashboard-set-heading-icons t
+        dashboard-set-file-icons    t
+        dashboard-center-content    t)
   (dashboard-setup-startup-hook))
 
 (provide 'cool-dashboard)
